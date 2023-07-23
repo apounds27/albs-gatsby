373da6d9117a4d001857b51c72ccbff7e986b9c1
apounds <apounds@bethel.jw.org>
2023-07-22 21:34:55 -0500

    add .env token

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,61 +7,65 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
-module.exports = {
-  siteMetadata: {
-    title: `A Discovery of Gatsby`,
-    author: {
-      name: `Ashley Pounds`,
-      summary: `who loves to learn and travel. Someone send me to Banff!`,
-    },
-    description: `A starter blog demonstrating how an API could populate this blog.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
-    social: {
-      twitter: ``,
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+export const siteMetadata = {
+  title: `A Discovery of Gatsby`,
+  author: {
+    name: `Ashley Pounds`,
+    summary: `who loves to learn and travel. Someone send me to Banff!`,
+  },
+  description: `A starter blog demonstrating how an API could populate this blog.`,
+  siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+  social: {
+    twitter: ``,
+  },
+};
+export const plugins = [
+  `gatsby-plugin-image`,
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      path: `${__dirname}/content/blog`,
+      name: `blog`,
     },
   },
-  plugins: [
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `images`,
+      path: `${__dirname}/src/images`,
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 630,
-            },
+  },
+  {
+    resolve: `gatsby-transformer-remark`,
+    options: {
+      plugins: [
+        {
+          resolve: `gatsby-remark-images`,
+          options: {
+            maxWidth: 630,
           },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
+        },
+        {
+          resolve: `gatsby-remark-responsive-iframe`,
+          options: {
+            wrapperStyle: `margin-bottom: 1.0725rem`,
           },
-          `gatsby-remark-prismjs`,
-        ],
-      },
+        },
+        `gatsby-remark-prismjs`,
+      ],
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        query: `
+  },
+  `gatsby-transformer-sharp`,
+  `gatsby-plugin-sharp`,
+  {
+    resolve: `gatsby-plugin-feed`,
+    options: {
+      query: `
           {
             site {
               siteMetadata {
@@ -73,20 +77,20 @@ module.exports = {
             }
           }
         `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.nodes.map(node => {
-                return Object.assign({}, node.frontmatter, {
-                  description: node.excerpt,
-                  date: node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + node.fields.slug,
-                  custom_elements: [{ "content:encoded": node.html }],
-                })
-              })
-            },
-            query: `{
+      feeds: [
+        {
+          serialize: ({ query: { site, allMarkdownRemark } }) => {
+            return allMarkdownRemark.nodes.map(node => {
+              return Object.assign({}, node.frontmatter, {
+                description: node.excerpt,
+                date: node.frontmatter.date,
+                url: site.siteMetadata.siteUrl + node.fields.slug,
+                guid: site.siteMetadata.siteUrl + node.fields.slug,
+                custom_elements: [{ "content:encoded": node.html }],
+              });
+            });
+          },
+          query: `{
               allMarkdownRemark(sort: {frontmatter: {date: DESC}}) {
                 nodes {
                   excerpt
@@ -101,36 +105,35 @@ module.exports = {
                 }
               }
             }`,
-            output: "/rss.xml",
-            title: "Gatsby Starter Blog RSS Feed",
-          },
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `Gatsby`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: `gatsby-source-graphql`,
-      options: {
-        typeName: `GitHub`,
-        fieldName: `github`,
-        url: `https://api.github.com/graphql`,
-        headers: {
-          Authorization: `Bearer your-github-token`,
+          output: "/rss.xml",
+          title: "Gatsby Starter Blog RSS Feed",
         },
+      ],
+    },
+  },
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `Gatsby Starter Blog`,
+      short_name: `Gatsby`,
+      start_url: `/`,
+      background_color: `#ffffff`,
+      // This will impact how browsers show your PWA/website
+      // https://css-tricks.com/meta-theme-color-and-trickery/
+      // theme_color: `#663399`,
+      display: `minimal-ui`,
+      icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+    },
+  },
+  {
+    resolve: `gatsby-source-graphql`,
+    options: {
+      typeName: `GitHub`,
+      fieldName: `github`,
+      url: `https://api.github.com/graphql`,
+      headers: {
+        Authorization: process.env.GITHUB_TOKEN,
       },
     },
-  ],
-}
+  },
+];
